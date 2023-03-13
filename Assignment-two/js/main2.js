7756d51a5febc2ad167813417037ae0c12f78be7
AnkithaSAmin <124131221+AnkithaSAmin@users.noreply.github.com>
2023-03-13 12:58:06 +0530

    Delete Assignment-two directory

--- Assignment-two/js/main2.js
+++
@@ -1,9 +0,0 @@
-$('.responsive').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    dots: true,
-    prevArrow: false,
-    nextArrow: false,
-  });
